6af8d1df720f1b5b974de4e87c58955bdaedb3e0
mukaj <43552866+mukaj@users.noreply.github.com>
2018-10-04 16:50:47 +0200

    Update Website
    Updated the website that was used for sentence generation
    Changed python version to 3.x

--- script.py
+++ script.py
@@ -1,15 +1,19 @@
 from bs4 import BeautifulSoup
-import urllib2
+from urllib.request import urlopen
 
-web_link = 'https://www.merriam-webster.com/dictionary/'
+web_link = 'https://www.dictionary.com/browse/'
 word_list = open("input.txt","r")
 sentence_list = open("output.txt", "w")
 for line in word_list:
 	try:
-		page = urllib2.urlopen(web_link + line.strip())
+		page = urlopen(web_link + line.strip())
 		soup = BeautifulSoup(page, 'html.parser')
-		sentence_bx = soup.find(class_="definition-inner-item")
+		sentence_bx = soup.find(class_="css-1khtv86 e1rg2mtf2")
+		sentence = sentence_bx.text.strip()
+		sentence_list.write(line.strip() + " - " + sentence + " - ")
+		sentence_bx = soup.find(class_="css-it69we e15kc6du7")
 		sentence = sentence_bx.text.strip()
 		sentence_list.write(sentence + "\n")
 	except:
-		print line + " - word was not found\n"
+		print (line + " - word was not found\n")
+sentence_list.close()
